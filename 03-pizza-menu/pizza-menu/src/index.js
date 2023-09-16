7c14fead9334433508b39229b2f3f74dec46233e
Kavin Abregana <abreganakavin@gmail.com>
2023-09-17 04:38:18 +0800

    conditional rendering ternary

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -72,18 +72,21 @@ function Header() {
 function Menu() {
   const pizzas = pizzaData;
   const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
       {/* in conditional rendering, always use true or false, intead of condition that returns a number or zero */}
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             // passing each object
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our Menu. Please come back later.</p>
       )}
 
       {/* <Pizza
@@ -98,7 +101,7 @@ function Menu() {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 0;
+  const openHour = 5;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
@@ -106,11 +109,15 @@ function Footer() {
   return (
     <footer className="footer">
       {/* conditional rendering */}
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
       )}
     </footer>
   );
