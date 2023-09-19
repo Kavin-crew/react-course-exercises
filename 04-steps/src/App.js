79199eff6bd40b2034d1475a8648f9fd293fa143
Kavin Abregana <abreganakavin@gmail.com>
2023-09-20 03:01:25 +0800

    event handlers

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -7,6 +7,13 @@ const messages = [
 export default function App() {
   const step = 1;
 
+  function handlePrevious() {
+    alert('prev');
+  }
+  function handleNext() {
+    alert('next');
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -20,10 +27,16 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
