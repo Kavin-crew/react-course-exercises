ad457033fb2ae1aea56932282c8f4ad467430b7a
Kavin abregana <kavin.abregana@proweaver.net>
2023-09-24 03:28:47 +0800

    controlled elements

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -20,21 +22,45 @@ function Logo() {
 }
 
 function Form() {
+  // controlled elements has 3 steps (state, value, onChange)
+  // 1. state
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, id: Date.now() };
+    initialItems.push(newItem);
+    console.log(initialItems);
+
+    setDescription('');
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={e => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      {/* 2. set input value to state Value */}
+      {/* 3. add onChange update the state*/}
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
