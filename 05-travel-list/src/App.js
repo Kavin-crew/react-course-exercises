701bb7fe3c42feac7c3a6d7cc010f49379480d75
Kavin abregana <kavin.abregana@proweaver.net>
2023-09-24 01:27:06 +0800

    adding list

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -10,7 +15,7 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1>🏖️ Far Away 💼</h1>;
+  return <h1>🏝️ Far Away 💼</h1>;
 }
 
 function Form() {
@@ -22,7 +27,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.description}: {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
