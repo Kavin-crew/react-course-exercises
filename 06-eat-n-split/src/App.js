49ed5e8f52d2fa9159ab563b4c2ac991b6a33ba2
Kavin Abregana <kavin.abregana@proweaver.net>
2023-09-28 18:57:18 +0800

    bu

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -89,13 +89,30 @@ function Button({ children, onClick }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('https://i.pravatar.cc/48?u=499476');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const id = crypto.randomUUID();
+
+    const newFriend = { id, name, image: `${image}?=${id}`, balance: 0 };
+
+    console.log(newFriend);
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🧑‍🤝‍🧑Friend name</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
 
       <label>🖼️ Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={e => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
