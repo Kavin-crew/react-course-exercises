046e93ff0c8dd14314c4051451d219b965b3355f
Kavin abregana <abreganakavin@gmail.com>
2023-10-06 21:56:52 +0800

    key prop

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -1,20 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const content = [
   {
-    summary: "React is a library for building UIs",
+    summary: 'React is a library for building UIs',
     details:
-      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      'Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    summary: "State management is like giving state a home",
+    summary: 'State management is like giving state a home',
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    summary: "We can think of props as the component API",
+    summary: 'We can think of props as the component API',
     details:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   },
 ];
 
@@ -39,7 +39,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
       ) : (
         <DifferentContent />
       )}
@@ -50,7 +53,7 @@ function Tabbed({ content }) {
 function Tab({ num, activeTab, onClick }) {
   return (
     <button
-      className={activeTab === num ? "tab active" : "tab"}
+      className={activeTab === num ? 'tab active' : 'tab'}
       onClick={() => onClick(num)}
     >
       Tab {num + 1}
@@ -72,8 +75,8 @@ function TabContent({ item }) {
       {showDetails && <p>{item.details}</p>}
 
       <div className="tab-actions">
-        <button onClick={() => setShowDetails((h) => !h)}>
-          {showDetails ? "Hide" : "Show"} details
+        <button onClick={() => setShowDetails(h => !h)}>
+          {showDetails ? 'Hide' : 'Show'} details
         </button>
 
         <div className="hearts-counter">
