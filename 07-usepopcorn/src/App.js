363ff7d303e2b58316bf0c2ad151665dab3e08e2
Kavin abregana <abreganakavin@gmail.com>
2023-10-22 19:13:42 +0800

    saving watched list to localstorage

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -9,11 +9,16 @@ const KEY = '4a287b95';
 
 export default function App() {
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   const [query, setQuery] = useState('');
   const [selectedId, setSelectedId] = useState(null);
+
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem('watched');
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectMovie(id) {
     setSelectedId(selectedId => (id === selectedId ? null : id));
@@ -30,6 +35,13 @@ export default function App() {
   function handDeleteWatched(id) {
     setWatched(watched => watched.filter(movie => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem('watched', JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
