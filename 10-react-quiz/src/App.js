a81d842b50029c3bbf3cf07d129e94eafe54e55e
Kavin abregana <abreganakavin@gmail.com>
2023-10-25 23:43:28 +0800

    new exercise  with useReducer

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,5 +1,48 @@
-import DateCounter from './DateCounter';
+import { useEffect } from 'react';
+import Header from './Header';
+import Main from './Main';
+import { useReducer } from 'react';
+
+const initialState = {
+  questions: [],
+  status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished'
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 export default function App() {
-  return <DateCounter />;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    try {
+      async function getData() {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      }
+      getData();
+    } catch (error) {
+      dispatch({ type: 'dataFailed' });
+      console.log('error');
+    }
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
