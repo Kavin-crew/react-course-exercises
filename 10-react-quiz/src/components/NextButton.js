19e336361a54097c5aad9ce5bafa6e84857a96e0
Kavin abregana <abreganakavin@gmail.com>
2023-10-27 22:34:00 +0800

    adding next question button

---
+++ 10-react-quiz/src/components/NextButton.js
@@ -0,0 +1,16 @@
+import { type } from '@testing-library/user-event/dist/type';
+
+function NextButton({ dispatch, answer }) {
+  if (answer === null) return null;
+
+  return (
+    <button
+      className="btn btn-ui"
+      onClick={() => dispatch({ type: 'nextQuestion' })}
+    >
+      Next
+    </button>
+  );
+}
+
+export default NextButton;
