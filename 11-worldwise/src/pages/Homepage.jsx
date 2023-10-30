fd18e7dd41bb024f58f67288de293c8ec503cddc
Kavin abregana <abreganakavin@gmail.com>
2023-10-30 13:55:07 +0800

    implementing basic routes

---
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,5 @@
+function Homepage() {
+  return <div>WorldWise</div>;
+}
+
+export default Homepage;
---
+++ 11-worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,5 @@
+function PageNotFound() {
+  return <div>Error404: Page Not Found</div>;
+}
+
+export default PageNotFound;
