6ae773e9ee69baabbf3716da7e74a91396787a49
Kavin abregana <abreganakavin@gmail.com>
2023-10-30 14:40:52 +0800

    css modules global classes

--- 11-worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/src/pages/Homepage.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>WorldWise</h1>
 
-      <Link to="/pricing">Pricing</Link>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
