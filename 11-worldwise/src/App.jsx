7c8c503f032c8684a70d8be2021742bf68d119f0
Kavin Abregana <kavin.abregana@proweaver.net>
2023-10-31 18:17:16 +0800

    nested routes and index

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,20 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import AppLayout from "./pages/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
+import AppLayout from './pages/AppLayout';
+import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- 11-worldwise/src/components/Sidebar.jsx
+++ 11-worldwise/src/components/Sidebar.jsx
@@ -1,7 +1,8 @@
-import AppNav from "./AppNav";
-import Footer from "./Footer";
-import Logo from "./Logo";
-import styles from "./Sidebar.module.css";
+import { Outlet } from 'react-router-dom';
+import AppNav from './AppNav';
+import Footer from './Footer';
+import Logo from './Logo';
+import styles from './Sidebar.module.css';
 
 function Sidebar() {
   return (
@@ -9,7 +10,7 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
       <Footer />
     </div>
   );
