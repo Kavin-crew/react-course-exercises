992ff366ff81ac5b749b85d1a8c1eca9a8496c18
Kavin abregana <abreganakavin@gmail.com>
2023-11-02 22:09:04 +0800

    navigate in route

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 /*eslint no-unused-vars: "warn"*/
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -43,10 +43,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
