5e145a460893ad6ad6c69f26f3884c4d0a011449
Kavin <abreganakavin@gmail.com>
2023-11-13 23:13:13 +0800

    redirect non- authenticated

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -14,6 +14,7 @@ import Form from "./components/Form";
 import CountryList from "./components/CountryList";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -25,7 +26,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
