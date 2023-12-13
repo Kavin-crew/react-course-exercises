57affd2f1fbc687b1375661b90a78f4d44262640
Kavin Abregana <kavin.abregana@proweaver.net>
2023-12-13 17:44:32 +0800

    steps on creating params

--- 11-worldwise/src/review/reducer.jsx
+++ 11-worldwise/src/review/reducer.jsx
@@ -1,17 +1,17 @@
-import { useReducer } from "react";
-import Product from "../pages/Product";
+import { useReducer } from 'react';
+import Product from '../pages/Product';
 
 // creating a reducer function
 function reducer(state, action) {
   switch (action.type) {
-    case "dec":
+    case 'dec':
       return { ...state, count: state.count - step };
-    case "setCount":
+    case 'setCount':
       return { ...state, count: action.payload };
-    case "setStep":
+    case 'setStep':
       return { ...state, step: action.payload };
     default:
-      throw new Error("Unknown action");
+      throw new Error('Unknown action');
   }
 }
 
@@ -23,18 +23,18 @@ const [{ count, step }, dispatch] = useReducer(reducer, initialState);
 
 // to call the reducer
 // for dec
-dispatch({ type: "dec" });
+dispatch({ type: 'dec' });
 
 // for manually setting the count
-dispatch({ type: "setCount", payload: Number(e.target.value) });
+dispatch({ type: 'setCount', payload: Number(e.target.value) });
 
 // for manually setting the step
-dispatch({ type: "setStep", payload: Number(e.target.value) });
+dispatch({ type: 'setStep', payload: Number(e.target.value) });
 
 ////////////////////////////////////////////
 //Routes
 ////////////////////////////////////////////
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
@@ -77,3 +77,17 @@ function App() {
 // :global(.test){
 //   background-color: red;
 // }
+
+////////////////////////////////////////////
+// Storing state in the URL
+////////////////////////////////////////////
+// Params - to pass data to next page
+// Query String - Global state that will be accessable everywhere
+
+// step 1. create a new route
+<Route index path="cities/:id" element={<City>City</City>} />;
+// step 2. link to that route
+<Link to={`${id}`}>Click Here</Link>;
+// step 3. read the state for the url
+import { useParams } from 'react-router-dom';
+const { id } = useParams();
