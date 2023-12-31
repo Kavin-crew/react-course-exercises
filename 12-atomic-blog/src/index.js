38c7777df729614aed1f57022a8d1ca4ce8b7620
Kavin Abregana <abreganakavin@gmail.com>
2024-01-01 02:33:22 +0800

    introduction of memo

--- 12-atomic-blog/src/index.js
+++ 12-atomic-blog/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./style.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import App from './App-memo';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
