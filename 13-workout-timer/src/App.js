1bbb1c596ec832c2347a4c0f0baecb940e964e3a
Kavin Abregana <abreganakavin@gmail.com>
2024-01-11 01:38:29 +0800

    inc and dec button

--- 13-workout-timer/src/App.js
+++ 13-workout-timer/src/App.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
-import Calculator from "./Calculator";
-import ToggleSounds from "./ToggleSounds";
-import { useMemo } from "react";
+import { useEffect, useState } from 'react';
+import Calculator from './Calculator';
+import ToggleSounds from './ToggleSounds';
+import { useMemo } from 'react';
 
 function formatTime(date) {
-  return new Intl.DateTimeFormat("en", {
-    month: "short",
-    year: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
+  return new Intl.DateTimeFormat('en', {
+    month: 'short',
+    year: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
   }).format(date);
 }
 
@@ -23,24 +23,24 @@ function App() {
   const workouts = useMemo(() => {
     return [
       {
-        name: "Full-body workout",
-        numExercises: partOfDay === "AM" ? 9 : 8,
+        name: 'Full-body workout',
+        numExercises: partOfDay === 'AM' ? 9 : 8,
       },
       {
-        name: "Arms + Legs",
+        name: 'Arms + Legs',
         numExercises: 6,
       },
       {
-        name: "Arms only",
+        name: 'Arms only',
         numExercises: 3,
       },
       {
-        name: "Legs only",
+        name: 'Legs only',
         numExercises: 4,
       },
       {
-        name: "Core only",
-        numExercises: partOfDay === "AM" ? 5 : 4,
+        name: 'Core only',
+        numExercises: partOfDay === 'AM' ? 5 : 4,
       },
     ];
   }, [partOfDay]);
--- 13-workout-timer/src/Calculator.js
+++ 13-workout-timer/src/Calculator.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import clickSound from "./ClickSound.m4a";
-import { memo } from "react";
+import { useEffect, useState } from 'react';
+import clickSound from './ClickSound.m4a';
+import { memo } from 'react';
 
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
@@ -8,7 +8,13 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const [duration, setDuration] = useState(0);
+
+  useEffect(() => {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+  }, []);
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
@@ -18,13 +24,20 @@ function Calculator({ workouts, allowSound }) {
     sound.play();
   };
 
+  function handleInc() {
+    setDuration(duration => Math.floor(duration) + 1);
+  }
+  function handleDec() {
+    setDuration(duration => (duration > 0 ? Math.ceil(duration) - 1 : 0));
+  }
+
   return (
     <>
       <form>
         <div>
           <label>Type of workout</label>
-          <select value={number} onChange={(e) => setNumber(+e.target.value)}>
-            {workouts.map((workout) => (
+          <select value={number} onChange={e => setNumber(+e.target.value)}>
+            {workouts.map(workout => (
               <option value={workout.numExercises} key={workout.name}>
                 {workout.name} ({workout.numExercises} exercises)
               </option>
@@ -38,7 +51,7 @@ function Calculator({ workouts, allowSound }) {
             min="1"
             max="5"
             value={sets}
-            onChange={(e) => setSets(e.target.value)}
+            onChange={e => setSets(e.target.value)}
           />
           <span>{sets}</span>
         </div>
@@ -50,7 +63,7 @@ function Calculator({ workouts, allowSound }) {
             max="180"
             step="30"
             value={speed}
-            onChange={(e) => setSpeed(e.target.value)}
+            onChange={e => setSpeed(e.target.value)}
           />
           <span>{speed} sec/exercise</span>
         </div>
@@ -61,19 +74,19 @@ function Calculator({ workouts, allowSound }) {
             min="1"
             max="10"
             value={durationBreak}
-            onChange={(e) => setDurationBreak(e.target.value)}
+            onChange={e => setDurationBreak(e.target.value)}
           />
           <span>{durationBreak} minutes/break</span>
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
-          {mins < 10 && "0"}
-          {mins}:{seconds < 10 && "0"}
+          {mins < 10 && '0'}
+          {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
