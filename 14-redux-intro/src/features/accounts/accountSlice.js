68c2b470deca9a347f529d639ecc72c3228b341f
Kavin Abregana <kavin.abregana@proweaver.net>
2024-01-16 18:13:48 +0800

    creating a slice in redux

--- 14-redux-intro/src/features/accounts/accountSlice.js
+++ 14-redux-intro/src/features/accounts/accountSlice.js
@@ -1,11 +1,43 @@
-const initialStateAccount = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
   isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
+const accountSlice = createSlice({
+  name: 'account',
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan(state, action) {
+      if (state.loan > 0) return;
+
+      state.loan = action.payload.amount;
+      state.loanPurpose = action.payload.purpose;
+      state.balance = state.balance + action.payload.amount;
+    },
+    payLoan(state, action) {
+      state.loan = 0;
+      state.loanPurpose = '';
+      state.balance -= state.loan;
+    },
+  },
+});
+
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export default accountSlice.reducer;
+
+/*
+export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case 'account/deposit':
       return {
@@ -76,3 +108,4 @@ export function payLoan() {
     type: 'account/payLoan',
   };
 }
+*/
