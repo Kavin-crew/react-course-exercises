f32e9eb8b9ce5d4e08a954d2e7a548d0e9bbb2f7
Kavin abregana <abreganakavin@gmail.com>
2024-01-16 22:54:12 +0800

    convert acountslice to redux toolkit

--- 14-redux-intro/src/features/accounts/accountSlice.js
+++ 14-redux-intro/src/features/accounts/accountSlice.js
@@ -17,17 +17,28 @@ const accountSlice = createSlice({
     withdraw(state, action) {
       state.balance -= action.payload;
     },
-    requestLoan(state, action) {
-      if (state.loan > 0) return;
 
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-      state.balance = state.balance + action.payload.amount;
+    // preparing the action before it reaches the reducer
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+
+      reducer(state, action) {
+        if (state.loan > 0) return;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
     },
+
     payLoan(state, action) {
+      state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = '';
-      state.balance -= state.loan;
     },
   },
 });
