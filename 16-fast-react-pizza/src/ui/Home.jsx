d8fa3334b7407a685bd8cb83a74c99d3a141ae7a
Kavin Abregana <abreganakavin@gmail.com>
2024-01-23 21:36:07 +0800

    tailwind installation

--- 16-fast-react-pizza/src/ui/Home.jsx
+++ 16-fast-react-pizza/src/ui/Home.jsx
@@ -1,13 +1,13 @@
 function Home() {
-  return (
-    <div>
-      <h1>
-        The best pizza.
-        <br />
-        Straight out of the oven, straight to you.
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-yellow-500 font-semibold text-center text-xl">
+                The best pizza.
+                <br />
+                Straight out of the oven, straight to you.
+            </h1>
+        </div>
+    );
 }
 
 export default Home;
