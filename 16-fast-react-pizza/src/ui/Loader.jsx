8dc94829dfb129ae84423fe489f42fcf32d54381
Kavin Abregana <abreganakavin@gmail.com>
2024-01-25 22:50:26 +0800

    updating loader

--- 16-fast-react-pizza/src/ui/Loader.jsx
+++ 16-fast-react-pizza/src/ui/Loader.jsx
@@ -1,5 +1,9 @@
 function Loader() {
-    return <div className="loader"></div>;
+    return (
+        <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm flex justify-center items-center">
+            <div className="loader"></div>;
+        </div>
+    );
 }
 
 export default Loader;
