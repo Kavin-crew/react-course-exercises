0664af91254e85cdc513189fe69e913c2d9ed9eb
Kavin Abregana <abreganakavin@gmail.com>
2024-01-28 01:31:27 +0800

    styling order form

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -48,21 +48,21 @@ function CreateOrder() {
             <Form method="POST">
                 <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
                     <label className="sm:basis-40">First Name</label>
-                    <input className="input" type="text" name="customer" required />
+                    <input className="input grow" type="text" name="customer" required />
                 </div>
 
                 <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
-                    <label>Phone number</label>
+                    <label className="sm:basis-40">Phone number</label>
                     <div className="grow">
-                        <input className="input" type="tel" name="phone" required />
-                        {formErrors?.phone && <p>{formErrors.phone}</p>}
+                        <input className="input w-full" type="tel" name="phone" required />
+                        {formErrors?.phone && <p className="mt-2 rounded-full bg-red-200 text-red-700 text-sm px-2 py-1">{formErrors.phone}</p>}
                     </div>
                 </div>
 
                 <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
-                    <label>Address</label>
+                    <label className="sm:basis-40">Address</label>
                     <div className="grow">
-                        <input className="input" type="text" name="address" required />
+                        <input className="input w-full" type="text" name="address" required />
                     </div>
                 </div>
 
@@ -75,7 +75,9 @@ function CreateOrder() {
                         // value={withPriority}
                         // onChange={(e) => setWithPriority(e.target.checked)}
                     />
-                    <label htmlFor="priority">Want to yo give your order priority?</label>
+                    <label htmlFor="priority" className="font-medium">
+                        Want to yo give your order priority?
+                    </label>
                 </div>
 
                 <div>
--- 16-fast-react-pizza/src/ui/Button.jsx
+++ 16-fast-react-pizza/src/ui/Button.jsx
@@ -6,13 +6,13 @@ import { Link } from 'react-router-dom';
 /* eslint-disable react/prop-types */
 function Button({ children, disabled, to, type }) {
     const base =
-        'rounded-full bg-yellow-500  font-semibold uppercase tracking-wide text-stone-800 hover:bg-yellow-300 transition-all duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed inline-block ';
+        'rounded-full text-sm bg-yellow-500  font-semibold uppercase tracking-wide text-stone-800 hover:bg-yellow-300 transition-all duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed inline-block ';
 
     const styles = {
         primary: base + ' px-4 py-3 md:px-6 md:py-4',
         small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
         secondary:
-            'rounded-full font-semibold uppercase tracking-wide text-stone-400 hover:bg-stone-400 hover:text-stone-800 focus:text-stone-800 transition-all duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed inline-block border-2 border-stone-400 px-4 py-3 md:px-6 md:py-4',
+            'rounded-full text-sm font-semibold uppercase tracking-wide text-stone-400 hover:bg-stone-400 hover:text-stone-800 focus:text-stone-800 transition-all duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed inline-block border-2 border-stone-400 px-4 py-3 md:px-6 md:py-4',
     };
 
     if (to)
