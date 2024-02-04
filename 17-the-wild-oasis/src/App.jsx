ee7c0eaf2382a82fc0f3ca916bd980f46e54b8ef
Kavin Abregana <abreganakavin@gmail.com>
2024-02-04 23:20:53 +0800

    setting up react query

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -9,10 +9,21 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -29,7 +40,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
---
+++ supabase-notes.sql
@@ -0,0 +1,64 @@
+-- note
+-- in fixing the curl or pasting the curl request,
+-- 1. change single quotes to double quotes
+-- 2. change / to ^
+
+npm i @supabase/supabase-js
+------------------------------------------------
+-- create file supabase.js
+------------------------------------------------
+
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://bdvhiizjigklgnlfzxfc.supabase.co";
+-- change the process.env.SUPABASE_KEY to our own key in settings>api
+-- const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkdmhpaXpqaWdrbGdubGZ6eGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4ODU4MDIsImV4cCI6MjAyMjQ2MTgwMn0.Si3nBLq2HM3WlEBStccJOSjloJmq37auchTeRdMt_rU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
+
+------------------------------------------------
+-- trying to read the rows in the column
+------------------------------------------------
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin cannot be loaded");
+  }
+
+  return data;
+}
+
+------------------------------------------------
+-- to start with react query
+------------------------------------------------
+-- 1. create place where the data lives
+-- 2. we provide that to application
+
+npm i @tanstack/react-query@4 @tanstack/react-query-devtools --force
+
+-- in app.js
+import { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
+function App() {
+  return (
+      <QueryClientProvider client={queryClient}>
+          -- for react query devtools component
+          <ReactQueryDevtools initialIsOpen={false} />
+          <Route path="dashboard" element={<Dashboard />} />
+      </QueryClientProvider>
+  );
+}
