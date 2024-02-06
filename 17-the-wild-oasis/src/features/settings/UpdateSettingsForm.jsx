b8b543bab5fa6cb93079b9f39129aa023d1fd20f
Kavin Abregana <abreganakavin@gmail.com>
2024-02-07 01:01:03 +0800

    getching data for settings

--- 17-the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
+++ 17-the-wild-oasis/src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,43 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import { useSettings } from "./useSettings";
+import Spinner from "../../ui/Spinner";
 
 function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestPerBooking,
+      breakfastPrice,
+    } = {},
+  } = useSettings();
+
+  if (isLoading) return <Spinner />;
+
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow label="Maximum nights/booking">
+        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow label="Maximum guests/booking">
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={maxGuestPerBooking}
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price">
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfastPrice}
+        />
       </FormRow>
     </Form>
   );
