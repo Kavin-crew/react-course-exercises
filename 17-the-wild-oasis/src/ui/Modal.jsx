94d7db409f70b0dbf9ca418d1cfe62cf4fc420c8
Kavin abregana <abreganakavin@gmail.com>
2024-02-08 16:07:02 +0800

    closing modal

--- 17-the-wild-oasis/src/ui/Modal.jsx
+++ 17-the-wild-oasis/src/ui/Modal.jsx
@@ -2,6 +2,7 @@ import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
 import { createPortal } from "react-dom";
 import { cloneElement, createContext, useContext, useState } from "react";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -77,11 +78,13 @@ function Open({ children, opens: openWindowName }) {
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
 
+  const { ref } = useOutsideClick(close);
+
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
