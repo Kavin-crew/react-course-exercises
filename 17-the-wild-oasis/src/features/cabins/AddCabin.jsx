083af88bef8b8377ba802f86c4c32a68c0a4bf75
Kavin Abregana <abreganakavin@gmail.com>
2024-02-08 17:31:44 +0800

    applying modal in editing and deleting cabin

--- 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,25 +1,19 @@
 import Modal from "../../ui/Modal";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
-import CabinTable from "./CabinTable";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button>View Table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
