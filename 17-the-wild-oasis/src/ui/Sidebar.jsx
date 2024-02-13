9a7fd86a12529800251a0c078ee56503ec632d83
Kavin Abregana <abreganakavin@gmail.com>
2024-02-13 15:13:27 +0800

    upload new data

--- 17-the-wild-oasis/src/ui/Sidebar.jsx
+++ 17-the-wild-oasis/src/ui/Sidebar.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
-import { useCabins } from "../features/cabins/useCabins";
+import Uploader from "../data/Uploader";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -21,6 +21,8 @@ function Sidebar() {
     <StyledSidebar>
       <Logo />
       <MainNav />
+
+      <Uploader />
     </StyledSidebar>
   );
 }
