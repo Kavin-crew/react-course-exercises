e55c8f97e369fec7e6cd0e8084ee1ee07852adf6
Kavin Abregana <abreganakavin@gmail.com>
2024-02-18 23:45:21 +0800

    change password

--- 17-the-wild-oasis/src/features/authentication/UpdatePasswordForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/UpdatePasswordForm.jsx
@@ -13,7 +13,7 @@ function UpdatePasswordForm() {
   const { updateUser, isUpdating } = useUpdateUser();
 
   function onSubmit({ password }) {
-    updateUser({ password }, { onSuccess: reset });
+    updateUser({ password }, { onSuccess: () => reset() });
   }
 
   return (
--- 17-the-wild-oasis/src/pages/Account.jsx
+++ 17-the-wild-oasis/src/pages/Account.jsx
@@ -1,6 +1,7 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm";
+import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
 
 function Account() {
   return (
@@ -12,9 +13,9 @@ function Account() {
         <UpdateUserDataForm />
       </Row>
 
-      <Row>
+      <Row type="vertical">
         <Heading as="h3">Update password</Heading>
-        <p>Update user password form</p>
+        <UpdatePasswordForm />
       </Row>
     </>
   );
