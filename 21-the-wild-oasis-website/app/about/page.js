c66878c8d84169a28feca2665c98a8d414d90480
Kavin Abregana <abreganakavin@gmail.com>
2025-02-25 01:50:56 +0800

    creating simple app routes

---
+++ 21-the-wild-oasis-website/app/about/page.js
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
+
+export default Page;
---
+++ 21-the-wild-oasis-website/app/account/page.js
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <div>
+      <h1>Account Page</h1>
+    </div>
+  );
+}
+
+export default Page;
