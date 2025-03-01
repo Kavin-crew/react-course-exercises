7330d7e7fc9d224a618959626631250913e49409
Kavin Abregana <abreganakavin@gmail.com>
2025-03-02 00:50:57 +0800

    testing fetching data on server component

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,7 +1,17 @@
-function Page() {
+async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+
   return (
     <div>
       <h1>Cabins Page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
