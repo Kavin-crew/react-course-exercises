9206e294df996a5a92fd6cd236e6f798208d6905
Kavin abregana <abreganakavin@gmail.com>
2025-03-11 19:41:43 +0800

    adding suspense component

---
+++ 21-the-wild-oasis-website/app/_components/CabinList.js
@@ -0,0 +1,15 @@
+import connectDB from "@/config/database";
+import Cabin from "@/models/Cabin";
+import CabinCard from "@/app/_components/CabinCard";
+
+export default async function CabinList() {
+  await connectDB();
+  const cabins = await Cabin.find({}).lean();
+
+  return (
+    <div className="grid grid-cols-2 gap-5">
+      {cabins.length > 0 &&
+        cabins.map((cabin) => <CabinCard cabin={cabin} key={cabin.name} />)}
+    </div>
+  );
+}
--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,20 +1,29 @@
-import connectDB from "@/config/database";
-import Cabin from "@/models/Cabin";
-import CabinCard from "@/app/_components/CabinCard";
+import CabinList from "@/app/_components/CabinList";
+import { Suspense } from "react";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 
 export default async function Page() {
-  await connectDB();
-  const cabins = await Cabin.find({}).lean();
-
   return (
-    <div className="grid grid-cols-2 gap-5">
-      {cabins.map((cabin) => (
-        <CabinCard cabin={cabin} key={cabin.name} />
-      ))}
+    <div>
+      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-primary-200 text-lg mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
